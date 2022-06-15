b108cdb895e67dbaff96b46544c251b3f5b3b054
akumaru <isakulovDev@gmail.com>
2022-06-15 14:08:28 +0500

    some changings

--- girlfriend/__init__.py
+++ girlfriend/__init__.py
@@ -1,4 +1,24 @@
 # Main Class
 
 class GirlFriend:
-    pass
+    def __init__(self):
+        self.name = "Karen"
+        self.age = "24"
+        self.is_hot = True
+        self.does_love_you = True
+        self.is_single = False
+        self.is_pretty = True
+        self.is_beautiful = True
+        self.is_cute = True
+        self.is_awesome = True
+        self.is_funny = True
+        self.is_sweet = True
+    
+    def kiss_me(self):
+        print("Kissing you")
+    
+    def hug_me(self):
+        print("Hugging you")
+
+    def introduce_yourself(self):
+        print("Hi, I'm " + self.name + " and I'm " + self.age + " years old.")
